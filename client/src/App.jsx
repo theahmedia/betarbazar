6a122a056b49a294cd14cbb0ee593c6c551e7643
The AH Media Devs Team <theahmediadevs@gmail.com>
2025-03-26 12:04:59 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,7 +34,7 @@ import AdminOrder from './components/dashboard/AdminOrderPage.jsx';
 /* Dashboard */
 import Products from './components/allproducts.jsx';
 import Category from './components/category.jsx';
-import CategoryProducts  from './components/CategoryProductPage.jsx';
+import CategoryProductsPage  from './components/CategoryProductPage.jsx';
 import Market2 from './components/allMarketPrice';
 import MainHotDeal from './components/allHotDeals.jsx';
 {/* CategoryMain Folders Pages */}
@@ -158,7 +158,7 @@ const App = () => {
         <Route path="/edit-user/:id" element={<EditUser />} />
         <Route path="/allproducts" element={<Products />} />
         <Route path="/category" element={<Category />} />
-        <Route path="/category/:categoryId" element={<CategoryProducts />} />
+        <Route path="/category/:categoryId" element={<CategoryProductsPage />} />
         {/* CategoryMain Folders Pages Path */}
         <Route path="/CategoryMain/dailyNeeds" element={<DailyNeeds />} />
         <Route path="/CategoryMain/fruits" element={<Fruities />} />
